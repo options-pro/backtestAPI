96b0ac712678599b2831a1007ef51fc720db7f96
Satyam Gupta <202051169@iiitvadodara.ac.in>
2022-11-08 17:17:12 +0530

    Created methods

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from project import views
 
+bt=views.backtest()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path("test",bt.test,name="test"),
+    path('ironcondor/',bt.ironcondor, name="ironcondor"),
+    path("shortstraddle/",bt.shorstraddle,name='shortstraddle'),
+    path('shortstrangle/',bt.shortstrangle, name="shortstrangle")
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from project.AlgoAshutosh import *
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 # Create your views here.
@@ -10,14 +12,48 @@ def home(request):
 
 
 class backtest:
-    def ironcondor(request):
-        object = ironCondor() 
-        object.ironCondorStrategy("datafiles/put-sample.csv","datafiles/call-sample(1).csv","datafiles/futures-sample.csv",100,100,200,200,1,10,50)
+    putFile="datafiles/put-sample.csv"
+    callFile="datafiles/call-sample(1).csv"
+    spotFile="datafiles/futures-sample.csv"
+
+    @csrf_exempt
+    def ironcondor(self,request):
         
-    def shorstraddle(request):
+        if request.method=='POST':
+            sellPut=request.POST.get('sellPut')
+            sellCall=request.POST.get('sellCall')
+            buyCall=request.POST.get('buyCall')
+            buyPut=request.POST.get('buyPut')
+            entry=request.POST.get('entry')
+            exit=request.POST.get('exit')
+            lotSize=request.POST.get('lotSize')        
+            object = ironCondor() 
+            profit=object.ironCondorStrategy(self.putFile,self.callFile,self.spotFile,sellPut,sellCall,buyCall,buyPut,entry,exit,lotSize)
+            jsonr=json.dumps(profit)
+            return JsonResponse(jsonr,safe=False)
+        elif request.method=="GET":
+            return HttpResponse("Error Happened")
+        
+
+    def shorstraddle(self,request):        
+        entry=request.POST.get('entry')
+        exit=request.POST.get('exit')
+        lotSize=request.POST.get('lotSize')
         object = shortStraddle()
-        object.shortStraddleStrategy()
+        profit=object.shortStraddleStrategy(self.putFile,self.callFile,self.spotFile,entry,exit,lotSize)
+        jsonr=json.dumps(profit)
+        return JsonResponse(jsonr,safe=False)
     
-    def shortstrangle(request):
+    def shortstrangle(self,request):
+        sellPut=request.POST.get('sellPut')
+        sellCall=request.POST.get('sellCall')
+        entry=request.POST.get('entry')
+        exit=request.POST.get('exit')
+        lotSize=request.POST.get('lotSize')
         object=shortStrangle()
-        object.shortStrangleStrategy()
+        profit=object.shortStrangleStrategy(self.putFile,self.callFile,self.spotFile,sellPut,sellCall,entry,exit,lotSize)
+        jsonr=json.dumps(profit)
+        return JsonResponse(jsonr,safe=False)
+    
+    def test(self,request):
+        return HttpResponse("Hello from test")
