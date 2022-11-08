6fd315aa45fb06650fb18e39d1c62a1ce4b9cb91
Satyam Gupta <202051169@iiitvadodara.ac.in>
2022-11-08 15:47:43 +0530

    removed algoashutosh package

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from AlgoAshutosh import *
+from project.AlgoAshutosh import *
 
 
 # Create your views here.
@@ -9,8 +9,15 @@ def home(request):
     return HttpResponse("Hello I am here")
 
 
-def backtest(request):
-    object = ironCondor() 
-    object.ironCondorStrategy("put-sample.csv","call-sample(1).csv","futures-sample.csv",100,100,200,200,1,10,50)
-
+class backtest:
+    def ironcondor(request):
+        object = ironCondor() 
+        object.ironCondorStrategy("datafiles/put-sample.csv","datafiles/call-sample(1).csv","datafiles/futures-sample.csv",100,100,200,200,1,10,50)
+        
+    def shorstraddle(request):
+        object = shortStraddle()
+        object.shortStraddleStrategy()
     
+    def shortstrangle(request):
+        object=shortStrangle()
+        object.shortStrangleStrategy()
